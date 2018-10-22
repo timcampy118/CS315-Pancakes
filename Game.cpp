b25b98e3b732c9eeeccdd3b6d9e588d054f99be8
Jay Khatri <jaykhatri@compute.cs.tamu.edu>
2018-10-22 17:34:04 -0500

    added to the hello world example

--- Game.cpp
+++ Game.cpp
@@ -3,8 +3,22 @@
 int main()
 {
 	initscr();
+
+	// Write a string to the screen.
 	printw("Hellow World");
+
+	// Get the key code of what the user typed.
+	int c = getch();
+
+	// Print the code to the screen.
+	printw("%d", c);
+
+	// Refresh the window to reflect the 'c' that's in memory.
 	refresh();
-	getch();
+
+	// This code is necessary for ending the window.
+	getch()
+	endwin();
+
 	return 0;
 }
