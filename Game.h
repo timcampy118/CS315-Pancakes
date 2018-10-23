d996d2f669ce0db46de0d26922b23a5bdddeea37
Zackary Ramirez <Zackary@Zackarys-MacBook-Pro.local>
2018-10-22 22:36:09 -0500

    added variables numOfPancakes and diff

--- Game.h
+++ Game.h
@@ -3,13 +3,16 @@
 
 #include "Player.h"
 #include "AI_Player.h"
+#include "Game_Window.h"
 
 
 class Game {
     private:
         Player player;
         AI_Player computer;
-
+        Game_Window window;
+        int numOfPancakes;
+        int diff;
 };
 
 #endif
